43644f2928f8a6b6a758055412d1da3d0527a520
Lillyb6 <113935001+Lillyb6@users.noreply.github.com>
2026-09-15 16:06:24 -0500

    Add test for three minus button presses

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,25 +3,37 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:plusandminus/main.dart';
 
 void main() {
-  testWidgets('Counter increments, decrements, and resets', (tester) async {
+  testWidgets('Counter reaches -3 after three minus presses', (tester) async {
     await tester.pumpWidget(const MyApp());
     expect(find.text('0'), findsNWidgets(2));
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-    expect(find.text('1'), findsOneWidget);
-
-    await tester.tap(find.byTooltip('Reset to zero'));
-    await tester.pump();
-    expect(find.text('0'), findsNWidgets(2));
-
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.tap(find.byIcon(Icons.remove));
     await tester.tap(find.byIcon(Icons.remove));
     await tester.pump();
-    expect(find.text('-1'), findsOneWidget);
 
-    await tester.tap(find.byTooltip('Reset to zero'));
-    await tester.pump();
-    expect(find.text('-1'), findsNothing);
+    expect(find.text('-3'), findsOneWidget);
+  });
+
+  testWidgets('Counter reaches 3 after three plus presses', (tester) async {
+    await tester.pumpWidget(const MyApp());
     expect(find.text('0'), findsNWidgets(2));
+
+    //await tester.tap(find.byIcon(Icons.add));
+    //await tester.pump();
+    //expect(find.text('3'), findsOneWidget);
+
+    //await tester.tap(find.byTooltip('Reset to zero'));
+    //await tester.pump();
+    //expect(find.text('0'), findsNWidgets(2));
+
+    //await tester.tap(find.byIcon(Icons.remove));
+    //await tester.pump();
+    //expect(find.text('-1'), findsOneWidget);
+
+    //await tester.tap(find.byTooltip('Reset to zero'));
+    //await tester.pump();
+    //expect(find.text('-1'), findsNothing);
+    //expect(find.text('0'), findsNWidgets(2));
   });
 }
